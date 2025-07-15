0f5d50dea2077f5792c10cdfa820092f431b4681
youngchan <youngchan@pcnict.com>
2025-07-15 17:39:26 +0900

    [init] pnpm monorepo 설정 - @monorepo/shared -> @monorepo/project-a 모듈공유 설정

--- apps/project-a/vite.config.ts
+++ apps/project-a/vite.config.ts
@@ -1,7 +1,17 @@
+import cesium from 'vite-plugin-cesium';
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cesium()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@monorepo/shared': path.resolve(__dirname, '../../packages/shared/src'),
+    },
+  },
 });
---
+++ packages/shared/src/03_features/index.ts
@@ -0,0 +1,1 @@
+export * from './CesiumMap/CesiumMap'
