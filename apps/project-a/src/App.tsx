052e7136bf440d4cf1bc849f96c7d574c087ba21
youngchan <youngchan@pcnict.com>
2025-07-17 14:57:27 +0900

    [dev] pnpm monorepo 설정 - 개별 프로젝트 tailwind 설정

--- apps/project-a/src/App.tsx
+++ apps/project-a/src/App.tsx
@@ -3,7 +3,14 @@ import type { ReactNode } from 'react';
 import './App.css';
 
 function App(): ReactNode {
-  return <div>프로젝트 A</div>;
+  return (
+    <div>
+      프로젝트 A
+      <button onClick={() => {}} className='bg-main'>
+        테스트버튼
+      </button>
+    </div>
+  );
   // return <Cesium3DModules />;
 }
 
---
+++ apps/project-a/tailwind.config.cjs
@@ -0,0 +1,12 @@
+// tailwind.config.cjs
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        main: '#414066',
+      },
+    },
+  },
+  plugins: [],
+};
