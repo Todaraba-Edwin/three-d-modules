c9e8a9f4fd68a55790bfaa4620c95d76e42055ca
youngchan <youngchan@pcnict.com>
2025-07-18 16:34:38 +0900

    [dev-shared] 리팩토링(Effect) 및 고도제한, 지도영역제한#2 독도, 이어도 포함

--- packages/shared/src/03_features/CesiumObj/02_hooks/useEffectCesiumBoundaryLimit.ts
+++ packages/shared/src/03_features/CesiumObj/02_hooks/useEffectCesiumBoundaryLimit.ts
@@ -11,9 +11,9 @@ export const useEffectCesiumBoundaryLimit = ({ viewer }: Props): void => {
     // 한국 영역 제한
     const KOREA_RECTANGLE = Cesium.Rectangle.fromDegrees(
       124.0,
-      36.0,
+      32.0,
       131.9,
-      40.0
+      42.0
     );
 
     const restrictCameraMovement = () => {
