424f8554c03ff0e044ef5516a8d164fa19acd52c
youngchan <youngchan@pcnict.com>
2025-07-29 16:42:46 +0900

    [dev-shared] gitpush.sh 메시지 반영 확인

--- apps/buildings/src/01_pages/CesiumRouter/Building/Building.tsx
+++ apps/buildings/src/01_pages/CesiumRouter/Building/Building.tsx
@@ -16,12 +16,7 @@ export const Building = (): ReactNode => {
     boundaryCoordinate,
   });
 
-  useSetGltfAsync({
-    viewer: viewerRef,
-    glbList,
-    boundaryCoordinate,
-    isFloor: true,
-  });
+  useSetGltfAsync({ viewer: viewerRef, glbList, boundaryCoordinate, isFloor: true,});
 
   useEffect(() => {
     setTimeout(() => {
