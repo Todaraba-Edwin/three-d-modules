0544054cb8341f39327074b2357c224afd7dc17d
youngchan <youngchan@pcnict.com>
2025-08-20 14:27:09 +0900

    NestJs 초기설정 #2, eslint 관련 상위로 설정 파일 이동

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,29 @@ import prettier from 'eslint-config-prettier';
 import eslintPluginReact from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import simpleImportSolt from 'eslint-plugin-simple-import-sort';
-import { globalIgnores } from 'eslint/config';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
 import path from 'path';
 import tseslint from 'typescript-eslint';
+
+const commonRules = {
+  // console
+  'no-console': ['warn', { allow: ['warn', 'error'] }], // console 사용시 경고, 프로덕션 코드에는 남지 않도록 경고 처리
+
+  // TS
+  '@typescript-eslint/no-explicit-any': 'warn', // any 타입 사용은 제한하여 타입 안전성 강화
+  '@typescript-eslint/explicit-module-boundary-types': 'warn', // 함수/컴포넌트 export 시 명시적 리턴 타입 권장
+
+  // etc
+  'prefer-const': 'warn', // 변경되지 않는 변수 const 권장
+  'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }], // 과도한 빈 줄 제거
+
+  // setting
+  'no-warning-comments': [
+    'warn',
+    { terms: ['todo'], location: 'start' }, // start: 줄 앞에 위치할 때만
+  ],
+};
 
 const utilsAddConfig = ({ projectPath, matchPath }) => ({
   files: [matchPath],
@@ -22,26 +40,12 @@ const utilsAddConfig = ({ projectPath, matchPath }) => ({
   },
   plugins: {
     react: eslintPluginReact,
-    'simple-import-sort': simpleImportSolt,
+    'simple-import-sort': simpleImportSort,
   },
   rules: {
-    // console
-    'no-console': ['warn', { allow: ['warn', 'error'] }], // console 사용시 경고, 프로덕션 코드에는 남지 않도록 경고 처리
-
-    // TS
-    '@typescript-eslint/no-explicit-any': 'warn', // any 타입 사용은 제한하여 타입 안전성 강화
-    '@typescript-eslint/explicit-module-boundary-types': 'warn', // 함수/컴포넌트 export 시 명시적 리턴 타입 권장
-
+    ...commonRules,
     // etc
     'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // 사용하지 않는 변수 경고, _로 시작하는 인자 무시
-    'prefer-const': 'warn', // 변경되지 않는 변수 const 권장
-    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }], // 과도한 빈 줄 제거
-
-    // setting
-    'no-warning-comments': [
-      'warn',
-      { terms: ['todo'], location: 'start' }, // start: 줄 앞에 위치할 때만
-    ],
   },
   extends: [
     js.configs.recommended, // no-unused-vars 여부포함
@@ -66,8 +70,31 @@ const utilsNodeConfig = () => {
   };
 };
 
+const utilsServerConfig = ({ projectPath, matchPath }) => ({
+  files: [matchPath],
+  languageOptions: {
+    parserOptions: {
+      project: [path.resolve(process.cwd(), projectPath)],
+      tsconfigRootDir: process.cwd(),
+    },
+    globals: globals.node,
+  },
+  rules: {
+    ...commonRules,
+  },
+  extends: [js.configs.recommended, tseslint.configs.recommended, prettier],
+});
+
 export default tseslint.config(
-  globalIgnores(['dist', 'node_modules']),
+  {
+    ignores: [
+      '**/.DS_Store',
+      '**/dist/**',
+      '**/node_modules/**',
+      '.pnpm-store/**',
+      '**/build/**',
+    ],
+  },
 
   // App 전용 코드
   utilsAddConfig({
@@ -82,8 +109,9 @@ export default tseslint.config(
     projectPath: './apps/buildings/tsconfig.app.json',
     matchPath: 'apps/buildings/**/*.{ts,tsx}',
   }),
-  utilsAddConfig({
+  utilsServerConfig({
     projectPath: './servers/server-a/tsconfig.json',
     matchPath: 'servers/server-a/**/*.ts',
   }),
+  utilsNodeConfig({})
 );
