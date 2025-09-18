97bedf4dc3a8a098e8d22163daff8969f820cd96
youngchan <youngchan@pcnict.com>
2025-09-18 14:15:18 +0900

    [dev-buildings] vscode snippets #2

--- apps/buildings/src/pages/AuthRouter/_wigets/_reactPortals/LoginActivityDetectedPortal.tsx
+++ apps/buildings/src/pages/AuthRouter/_wigets/_reactPortals/LoginActivityDetectedPortal.tsx
@@ -6,12 +6,13 @@ export const LoginActivityDetectedPortal = ({
 }: PropsWithChildren): ReactNode => {
   return createPortal(
     <div className='fixed top-0 left-0 w-full h-full z-50'>
-      // 배경 투명도, Default GRAY_600
+      {/* 배경 투명도, Default GRAY_600 */}
       <div
         className='w-full h-full bg-gray-600 opacity-40'
         onClick={() => {}}
       />
-      // Portal 본문관련
+
+      {/* Portal 본문관련 */}
       <div className='absolute w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-4'>
         <div className='bg-white p-4 rounded-2xl shadow-lg'>
           LoginActivityDetectedPortal
