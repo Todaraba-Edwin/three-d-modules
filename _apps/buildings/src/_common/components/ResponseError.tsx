8a8603fca9e2f2898fb037675f1984ee7168b1a2
youngchan <youngchan@pcnict.com>
2025-10-27 16:34:16 +0900

    [dev-buildings] 마이크로 에러 컴포넌트 리팩토링 ResponseError -> string 상수화

--- _apps/buildings/src/_common/components/ResponseError.tsx
+++ _apps/buildings/src/_common/components/ResponseError.tsx
@@ -3,6 +3,12 @@ import clsx from 'clsx';
 import { type PropsWithChildren, type ReactNode } from 'react';
 import { ErrorBoundary, type FallbackProps } from 'react-error-boundary';
 
+const DEFAULT_ERROR = {
+  HEADER: '문제가 발생했습니다.',
+  MESSAGE: '알 수 없는 에러가 발생했습니다.',
+  RETRY_BUTTON: '다시시도',
+};
+
 const fallbackRender = ({
   error,
   resetErrorBoundary,
@@ -10,7 +16,7 @@ const fallbackRender = ({
   const errorMessage =
     error instanceof Error && error.message
       ? error.message
-      : '알 수 없는 에러가 발생했습니다.';
+      : DEFAULT_ERROR.MESSAGE;
 
   return (
     <div
@@ -21,14 +27,14 @@ const fallbackRender = ({
         'rounded-lg border-2 border-dashed border-red-100'
       )}
     >
-      <h4 className='text-lg font-bold'>문제가 발생했습니다.</h4>
+      <h4 className='text-lg font-bold' children={DEFAULT_ERROR.HEADER} />
       <p className='my-2 text-sm'>{errorMessage}</p>
       <ButtonPrimary
-        onClick={resetErrorBoundary} // 다시 시도 버튼
+        onClick={resetErrorBoundary}
         fontSize='sm'
         size='sm'
         variant='tertiary'
-        children='다시시도'
+        children={DEFAULT_ERROR.RETRY_BUTTON}
       />
     </div>
   );
